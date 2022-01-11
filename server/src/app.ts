3a1fa943979d39c703ba35db7d8eba6b5a7bc0c1
Ville Koskela <vkoskela@gmail.com>
2022-01-11 15:28:16 +0200

    still working on the dev-environment

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,4 +5,10 @@ app.get('/', (_req, res) => {
   res.json({ msg: 'Hello World!' });
 });
 
+app.get('/api', (_req, res) => {
+  res.json({msg: 'Hello World! Thank you...'});
+});
+
+app.
+
 export default app;
