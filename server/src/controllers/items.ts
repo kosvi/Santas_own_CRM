bb868d8a81f0d6ddbf43d3e8ac0f5e45381f89c7
Ville Koskela <vkoskela@gmail.com>
2022-01-25 21:10:14 +0200

    minor fixes

--- server/src/controllers/items.ts
+++ server/src/controllers/items.ts
@@ -2,14 +2,15 @@ import express from 'express';
 import { listMostCommonItems } from '../services/itemService';
 import { ControllerError } from '../utils/customError';
 import { logger } from '../utils/logger';
-import { validateToString } from '../utils/validators';
+import { validateToNumber, validateToString } from '../utils/validators';
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
   let limit = 0;
   if (req.query.limit) {
     try {
-      limit = parseInt(validateToString(req.query.limit));
+      // We have to validate that parseInt didn't return NaN
+      limit = validateToNumber(parseInt(validateToString(req.query.limit)));
     } catch (error) {
       throw new ControllerError(400, 'limit has to be an integer');
     }
--- server/src/utils/customError.ts
+++ server/src/utils/customError.ts
@@ -1,6 +1,8 @@
+type StatusCode = 400 | 403 | 404 | 500;
+
 export class ControllerError extends Error {
   statusCode!: number;
-  constructor(statusCode: number, ...params: any) {
+  constructor(statusCode: StatusCode, ...params: any) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
     super(...params);
     this.name = 'ControllerError';
