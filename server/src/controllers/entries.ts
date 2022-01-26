96a64071e829765897f75aeeb923592971c0c175
Ville Koskela <vkoskela@gmail.com>
2022-01-26 20:59:21 +0200

    working on entries endpoint

--- server/src/controllers/entries.ts
+++ server/src/controllers/entries.ts
@@ -1,15 +1,23 @@
 import express from 'express';
-import { getEntriesByPersonId } from '../services/entryService';
+import { getLatestEntries } from '../services/entryService';
+import { validateToString } from '../utils/validators';
 const router = express.Router();
 
-router.get('/', (_req, res) => {
-  getEntriesByPersonId(1)
-    .then(response => {
-      console.log(response.count);
-      res.json(response.rows);
+router.get('/', (req, res, next) => {
+  let limit: string;
+  if (req.query.limit) {
+    limit = validateToString(req.query.limit);
+  } else {
+    // We can set this to anything we want as long as it doesn't parse int. 
+    // if parseInt return NaN and we call getLatestEntries with NaN, it's going to use default value
+    limit = '';
+  }
+  getLatestEntries(parseInt(limit))
+    .then(data => {
+      res.json(data);
     })
     .catch(error => {
-      console.error(error);
+      next(error);
     });
 });
 
--- server/src/services/entryService.ts
+++ server/src/services/entryService.ts
@@ -1,4 +1,7 @@
 import models from '../models';
+import { logger } from '../utils/logger';
+
+const MAX_NUMBER_OF_ENTRIES_RETURNED = 50;
 
 export const getEntriesByPersonId = async (id: number) => {
   const entries = models.Entry.findAndCountAll({
@@ -7,3 +10,18 @@ export const getEntriesByPersonId = async (id: number) => {
   return entries;
 };
 
+export const getLatestEntries = async (amount: number) => {
+  try {
+    const limit = Number.isNaN(amount) ? { limit: MAX_NUMBER_OF_ENTRIES_RETURNED } : { limit: Math.min(amount, MAX_NUMBER_OF_ENTRIES_RETURNED) };
+    const entries = models.Entry.findAll({
+      order: [
+        ['updated_at', 'DESC']
+      ],
+      ...limit
+    });
+    return entries;
+  } catch (error) {
+    logger.logError(error);
+    throw error;
+  }
+};
