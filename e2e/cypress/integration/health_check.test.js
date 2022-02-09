63b28ca48e35eb226240f44b52b1531cf02ca3f9
Ville Koskela <ville.koskela@kv1.fi>
2022-02-09 13:43:58 +0000

    remove outdated test

--- e2e/cypress/integration/health_check.test.js
+++ e2e/cypress/integration/health_check.test.js
@@ -1,10 +1,3 @@
-it('Version responds!', function () {
-  //  console.log('PORT:', Cypress.env('PORT'));
-  //  cy.visit(`${Cypress.env('FULL_HOST')}:${Cypress.env('PORT')}`);
-  cy.visit(Cypress.env('web_base'));
-  cy.contains('version');
-});
-
 it('Healthcheck works', function () {
   cy.visit(`${Cypress.env('api_base')}/version/health`);
   cy.contains('OK!');
